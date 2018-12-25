ecb71f7ccced0b8af8445f33d6bfb9f04bc95735
bogdanpasichnyk <pasechnikbs@gmail.com>
2018-12-25 17:58:21 +0200

    init

--- src/pages/Orders.vue
+++ src/pages/Orders.vue
@@ -1,6 +1,9 @@
 <template>
     <div>
-        <order-list-view v-for="order in orders" :key="order.id" :order="order"></order-list-view>
+        <div v-if="loading" class="loading">Loading...</div>
+        <div v-else class="orders-list">
+            <order-list-view v-for="order in orders" :key="order.id" :order="order"></order-list-view>
+        </div>
     </div>
 </template>
 
@@ -11,15 +14,20 @@
         components: {OrderListView},
         data() {
             return {
-                orders: []
+                orders: [],
+                loading: false,
             }
         },
         async created() {
+            this.loading = true
             this.orders = await this.$store.dispatch('orders/findAll')
+            this.loading = false
         }
     }
 </script>
 
 <style scoped>
-
+    .loading {
+        padding: 10px 5px;
+    }
 </style>
